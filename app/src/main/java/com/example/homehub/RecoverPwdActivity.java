7594e91d0437ad6a1bdfb2bdb8b094d46860d939
Laia Moñino Peñalva <laiamoninopenalva@a.cifpperedesongall.org>
2024-04-16 10:18:31 +0200

    Creación de las páginas principales del proyecto

---
+++ app/src/main/java/com/example/homehub/RecoverPwdActivity.java
@@ -0,0 +1,14 @@
+package com.example.homehub;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecoverPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_pwd);
+    }
+}
